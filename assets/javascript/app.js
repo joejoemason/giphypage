0ac6589eb73b0acaa5ff2825a0eedfb6b5238cc3
Joe Mason <joseph.mason101@gmail.com>
2019-04-04 00:24:16 -0400

    pushing finished assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,85 @@
-function createButton(name) {
-    let newButton = $(`<button class="btn btn-character" data-name="${name.toLowerCase()}">${name}</button>`);
-    $('#button-corral').append(newButton);
-    log(newButton);
-};
+$(document).ready(function () {
 
-let startButtonArray = ["New York", "Chicago", "Los Angeles", "Paris", "London", "Dubai", "Hong Kong", "Sydney", "Shanghai"];
+    var displayedButtons = ["Frosted Flakes", "Cinnamon Toast Crunch", "Raisin Brand Crunch"];
 
-$(document).on("click", '.btn-character', function () {
+    function displayImg() {
 
-    $('#gif-corral').empty();
-    $('#gif-corral').append(`<div id="input" class="footer text-center"></div>`);
-    createInput();
-    let queryName = $(this).attr("data-name");
-    let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + queryName + "+skylines&limit=10&api_key=SXhYMP6NZ2CRShj0Qb3Qs5p6x0fj984Q";
+        $("#display-images").empty();
+        var input = $(this).attr("data-name");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "cereal" + limit + "&api_key=CsnWh6UAofVZiQBTqqqWRyPsMesnMDZe";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+
+            for (var j = 0; j < limit; j++) {
+
+                var displayDiv = $("<div>");
+                displayDiv.addClass("holder");
+
+                var image = $("<img>");
+                image.attr("src", response.data[j].images.original_still.url);
+                image.attr("data-still", response.data[j].images.original_still.url);
+                image.attr("data-animate", response.data[j].images.original.url);
+                image.attr("data-state", "still");
+                image.attr("class", "gif");
+                displayDiv.append(image);
+
+                var rating = response.data[j].rating;
+                console.log(response);
+                var pRating = $("<p>").text("Rating: " + rating);
+                displayDiv.append(pRating)
+
+                $("#display-images").append(displayDiv);
+            }
+        });
+    }
+
+    function renderButtons() {
+
+        $("#display-buttons").empty();
+
+        for (var i = 0; i < displayedButtons.length; i++) {
+
+            var newButton = $("<button>")
+            newButton.attr("class", "btn btn-default");
+            newButton.attr("id", "input")
+            newButton.attr("data-name", displayedButtons[i]);
+            newButton.text(displayedButtons[i]);
+            $("#display-buttons").append(newButton);
+        }
+    }
+
+    function imageChangeState() {
+
+        var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+
+        if (state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        } else if (state == "animate") {
+            $(this).attr("src", stillImage);
+            $(this).attr("data-state", "still");
+        }
+    }
+
+    $("#submitPress").on("click", function () {
+
+        var input = $("#user-input").val().trim();
+        form.reset();
+        displayedButtons.push(input);
+
+        renderButtons();
+
+        return false;
+    })
+
+    renderButtons();
+
+    $(document).on("click", "#input", displayImg);
+    $(document).on("click", ".gif", imageChangeState);
+});
